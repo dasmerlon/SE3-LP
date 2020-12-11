273a4acc6f644efb852c556e8b6e87532f74da9d
Merle Hoffmann <hoffmannmerle@web.de>
2020-12-11 20:40:46 +0100

    add exercise 05

---
+++ 05/Hoffmann-Kiefer-Mutschinski_blatt05.pl
@@ -0,0 +1,42 @@
+/*
+SE3-LP WiSe 2020/2021
+Übung Gr.04:
+    Merle Hoffmann      (7031673)
+    Lukas Kiefer        (7260745)
+    Björn Mutschinski   (7029008)
+
+
+Aufgabenblatt 5:
+*/
+
+
+%%%Aufgabe 1:     
+
+%1.a:   
+
+
+%1.b: 
+
+
+%1.c:
+
+
+%1.d:
+
+
+%1.e:
+
+
+%1.f:
+
+
+%1.g:
+
+
+
+%%%Aufgabe 2: 
+
+%2.1:    
+    
+
+%2.2:
